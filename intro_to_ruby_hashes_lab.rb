6a03e664ea95a19ee36f69ec5ae2cf5e92c6f5cc
spectacular-system-6224 <d.leslie707@gmail.com>
2020-03-01 01:17:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,33 +1,34 @@
-def base_hash
-  # Remember implicit return! We're returning this new Hash _without_ the
-  # keyword "return." Nice and neat.
-	hash = Hash.new{}
-	return hash
-end
-
-p base_hash
-
-def monopoly_with_second_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-  p hash 
-  return hash
-end
-
-monopoly_with_second_tier
-
-def monopoly_with_third_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-end
-
-def monopoly_with_fourth_tier
-  # When you start writing the implementation for this method, copy the Hash
-  # you built in the previous method. Run the tests. They will fail, but
-  # they'll guide you in how to modify what you just did to have more
-  # complexity.
-end
+monopoly = {
+  :railroads =>
+    {
+      :pieces => 4,
+      :rent_in_dollars =>
+        {
+          :one_piece_owned => 25,
+          :two_piece_owned => 50,
+          :three_piece_owned => 100,
+          :four_piece_owned => 200
+        },
+        :names => 
+        {
+          :reading_railroads =>
+          {
+            "mortgage_value" => "$100"
+            },
+          :pennsylvania_railroads =>
+          {
+            "mortgage_value" => "$200"
+            },
+          :b_and_o_railroads =>
+          {
+            "mortgage_value" => "$400"
+            },
+          :shortline =>
+          {
+            "mortgage_value" => "$800"
+            }
+           }
+          }
+         }
+         
+p monopoly[:railroads][:rent_in_dollars]
